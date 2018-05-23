bb0f08cdaf5008296ac8d3e95c90fffae08fdfba
Pratyush271776508 <ppkuanr@gmail.com>
2018-05-23 22:35:54 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var fs = require('fs');
+var hbs = require('hbs');
+
+app = express();
+app.use('/assets',express.static('assets'));
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.get('/',(req,res)=>{
+  res.send('<p>This is just an index</p>');
+});
+app.get('/html',(req,res)=>{
+  res.sendFile(__dirname + '/home.html');
+});
+var obj = {age : 21,name: "Pratyush"};
+app.get('/render',(req,res)=>{
+  res.render('help.hbs',obj);
+});
+app.listen(3000,()=>{
+  console.log('server is starting');
+});
